40833b8960fee9059578354c0b39a5cff853df1a
Hardscrabble <tjmakunde@gmail.com>
2018-12-06 13:26:58 +0200

    Add files via upload
    new updates pre-app

--- app/php/enquirysender.php
+++ app/php/enquirysender.php
@@ -1,30 +1,28 @@
 <?php
 	// My modifications to mailer script from:
 	// Only process POST reqeusts.
-	$bride = $_GET['bride'];
-	$groom = $_GET['groom'];
-	$date = $_GET['date'];
-	$phone = $_GET['phone'];
+	$name = $_GET['name'];
 	$email = $_GET['email'];
 	$messagebody = $_GET['messagebody'];
 
 	// Set the from email address.
-	$from = "<webmaster@kaysimmzpix.co.zw>";
+	$from = "<webmaster@katfilms.co.zw>";
 
 	$subject = "Email Enquiry";
 
 	// Set the recipient email address.
     // FIXME: Update this to your desired email address.
-	$to = "info@kaysimmzpix.co.zw";
+	$to = "info@katfilms.co.zw";
 
 	// Build the email content.
 	$message = "
 				Hi,
 
-				A visitor has been sent a query from kaySIMMZpix.co.zw.
+				A visitor has been sent a query from katfilms.co.zw.
 
-				Name: $bride and $groom are getting married. They would likee to know if the date $date is available.
-				You may contact them on email: $email or phone number $phone.
+				Name: $name has sent a message to you from your website.
+				$messagebody.
+				$name may be contacted on the following email address: $email.
 
 				Yours Sincerely, Your website.
 				";
